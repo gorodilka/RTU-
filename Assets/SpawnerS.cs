90de0cbbdc0fa4771bbc1e11c84df3fc7bac959a
gorodilka <kaktysiktaty@gmail.com>
2019-11-22 11:11:54 +0300

    Initial commit

---
+++ Assets/SpawnerS.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerS : MonoBehaviour
+{
+
+    public GameObject ArrowPref;
+    public float force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject buffer = Instantiate<GameObject>(ArrowPref);
+            buffer.transform.position = gameObject.transform.position;
+            buffer.transform.rotation = gameObject.transform.rotation;
+            buffer.GetComponent<Rigidbody>().AddForce(gameObject.transform.forward.normalized * force);
+        }
+    }
+}
---
+++ Assets/scoreCounterS.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoreCounterS : MonoBehaviour
+
+
+{
+    float score;
+    public GameObject scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        float dist = 100 - (transform.position - collision.gameObject.transform.position).magnitude;
+        score += dist;
+        scoreText.GetComponent<TextMesh>().text = score.ToString();
+    }
+}
